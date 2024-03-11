4f9ebf26786d96dd5a3febb3761320033ec9b4fc
Idemudia <agbonghaeidemudia@gmail.com>
2024-03-11 09:37:19 +0100

    redis

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.asyncGet = promisify(this.client.get).bind(this.client);
+    this.asyncSet = promisify(this.client.set).bind(this.client);
+    this.client.on('error', (err) => {
+      console.log(err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const result = await this.asyncGet(key);
+    return result;
+  }
+
+  async set(key, value, duration) {
+    await this.asyncSet(key, value);
+    await this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
